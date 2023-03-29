950558b96a5a1cc8c82887f5a7154b9188fb6959
bilal-ahmed89 <ba1770490@gmail.com>
2023-03-30 02:53:38 +0500

    modified

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.route('/todos').get( getAllTodos);
 
-router.route('/todo/:id').get(isAuthenticatedUser, getTodoById);
+router.route('/todo/:id').get( getTodoById);
 
 router.route('/todo/update/:id').put(isAuthenticatedUser, updateTodo);
 
